9f67a3f4319ff18d69c2344028438809afd9750e
jimbabwe216 <31488448+jimbabwe216@users.noreply.github.com>
2018-03-19 09:34:14 -0600

    added battle class

---
+++ src/com/jimbabwe/controller/battle.java
@@ -0,0 +1,31 @@
+package com.jimbabwe.controller;
+
+public class battle {
+	
+	public battle() {
+		startBattle();
+		inBattle();
+	}
+	
+	public void startBattle() {
+		//ONLY RUN ONCE
+		//intro animation
+		//get enemy sprite
+		//get ally backsprite
+	}
+	
+	public void inBattle() {
+		//choose action
+		//check speed 
+		//run through attacks
+		//check if either hp is 0, if not, start from the top again
+	}
+	
+	public void endBattle() {
+		//only called by inBattle() if either you or trainer is out of pokemon
+		
+		//finishing trainer statement, award prizeMoney, award EXP
+	}
+	
+	
+}
