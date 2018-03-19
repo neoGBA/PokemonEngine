e5120882dd82a90fd3f9f025eed8c222e443af3d
jimbabwe216 <31488448+jimbabwe216@users.noreply.github.com>
2018-03-19 09:07:42 -0600

    added bulbasaur base stats

--- src/com/jimbabwe/pokemon/Bulbasaur.java
+++ src/com/jimbabwe/pokemon/Bulbasaur.java
@@ -1,5 +1,16 @@
 package com.jimbabwe.pokemon;
 
 public class Bulbasaur {
-
+	
+	int baseHP = 45;
+	int baseAttack = 49;
+	int baseDefense = 49;
+	int baseSpecialAttack = 65;
+	int baseSpecialDefense = 65;
+	int baseSpeed = 45;
+	
+	public Bulbasaur(int level) {
+		//determine stats based on base stats
+		//determine moves by level
+	}
 }
